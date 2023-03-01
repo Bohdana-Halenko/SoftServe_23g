407ea79dc62e23632f74051b779d33f7f1cf89a2
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2023-03-01 20:59:43 +0000

    added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY;
-  const initialHeight = window.innerWidth < 768 ? 120 : 70;
+  const initialHeight = window.innerWidth < 768 ? 70 : 45;
   if (scrollPosition >= initialHeight) {
     header.classList.add('fixed');
   } else {
